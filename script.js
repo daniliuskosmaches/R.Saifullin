c641c9343fc8b22a162e5f644de2cf9089ad2366
daniliuskosmaches <nasartuy@gmail.com>
2025-09-03 14:43:57 +0300

    рра

--- script.js
+++ script.js
@@ -1,135 +1,430 @@
 const API_BASE_URL = '/api';
 
 const charactersData = [
-  { name: "Капитан Америка", desc: "Капитан Бургера", image: "images/amurika.PNG", price: 5000, video: "videos/spiderman.mp4" },
-  { name: "Железный Человек", desc: "Гений, миллиардер, плейбой, филантроп", image: "images/tonysrark.PNG", price: 6000, video: "videos/ironman.mp4" },
-  { name: "Бэтгерл", desc: "Отважная героиня Готэма", image: "images/batgerrl.PNG", price: 5500, video: "videos/batgirl.mp4" },
-  { name: "Майнкрафт", desc: "Тюринг полная игра", image: "images/minicraftblyat.PNG", price: 4500, video: "videos/piero.mp4" },
-  { name: "Пьеро", desc: "Дед инсайд с ссср", image: "images/piero.PNG", price: 6500, video: "videos/elsa.mp4" },
-  { name: "Футболист", desc: "Отец роналдо", image: "images/ronaldo.PNG", price: 5000, video: "videos/pirate.mp4" },
-  { name: "Гарри Поттер", desc: "Юный волшебник из Хогвартса", image: "images/garypotter.PNG", price: 6000, video: "videos/harrypotter.mp4" },
-  { name: "Шрек", desc: "Кто проживает на дне болота", image: "images/shrek.PNG", price: 8000, video: "videos/tmnt.mp4" },
-  { name: "Супермен", desc: "Лунтик в плаще", image: "images/superpidor.PNG", price: 5500, video: "videos/mermaid.mp4" },
-  { name: "Фредди Фазбер", desc: "Поиграй с медведем", image: "images/fredybazbearholhoholhoohohhool.PNG", price: 5500, video: "videos/ninjago.mp4" },
-  { name: "Белоснежка", desc: "Самая добрая принцесса", image: "images/belosnezhka.PNG", price: 5500, video: "videos/snowwhite.mp4" },
-  { name: "Халк", desc: "Зеленный качок", image: "images/Halk.PNG", price: 5000, video: "videos/luntik.mp4" }
+  {
+    "name": "Капитан Америка",
+    "desc": "Капитан Бургера",
+    "image": "images/amurika.PNG",
+    "price": 5000
+  },
+  {
+    "name": "Barbie",
+    "desc": "Описание для Barbie",
+    "image": "images/barbie.PNG",
+    "price": 4702
+  },
+  {
+    "name": "Бэтгерл",
+    "desc": "Отважная героиня Готэма",
+    "image": "images/batgerrl.PNG",
+    "price": 5500
+  },
+  {
+    "name": "Batmen",
+    "desc": "Описание для Batmen",
+    "image": "images/batmen.PNG",
+    "price": 5714
+  },
+  {
+    "name": "Белоснежка",
+    "desc": "Самая добрая принцесса",
+    "image": "images/belosnezhka.PNG",
+    "price": 5500
+  },
+  {
+    "name": "Buzz",
+    "desc": "Описание для Buzz",
+    "image": "images/buzz.PNG",
+    "price": 5881
+  },
+  {
+    "name": "Cheburashka",
+    "desc": "Описание для Cheburashka",
+    "image": "images/cheburashka.PNG",
+    "price": 6535
+  },
+  {
+    "name": "Clown",
+    "desc": "Описание для Clown",
+    "image": "images/clown.PNG",
+    "price": 6208
+  },
+  {
+    "name": "Clowns",
+    "desc": "Описание для Clowns",
+    "image": "images/clowns.PNG",
+    "price": 7039
+  },
+  {
+    "name": "Djedai",
+    "desc": "Описание для Djedai",
+    "image": "images/djedai.PNG",
+    "price": 4513
+  },
+  {
+    "name": "Фредди Фазбер",
+    "desc": "Поиграй с медведем",
+    "image": "images/fredybazbearholhoholhoohohhool.PNG",
+    "price": 5500
+  },
+  {
+    "name": "Гарри Поттер",
+    "desc": "Юный волшебник из Хогвартса",
+    "image": "images/garypotter.PNG",
+    "price": 6000
+  },
+  {
+    "name": "Grinch",
+    "desc": "Описание для Grinch",
+    "image": "images/grinch.PNG",
+    "price": 5678
+  },
+  {
+    "name": "Халк",
+    "desc": "Зеленный качок",
+    "image": "images/Halk.PNG",
+    "price": 5000
+  },
+  {
+    "name": "Happy New Year",
+    "desc": "Описание для Happy New Year",
+    "image": "images/happy new year.PNG",
+    "price": 5802
+  },
+  {
+    "name": "Joker",
+    "desc": "Описание для Joker",
+    "image": "images/joker.PNG",
+    "price": 7930
+  },
+  {
+    "name": "Kreeper",
+    "desc": "Описание для Kreeper",
+    "image": "images/kreeper.PNG",
+    "price": 7642
+  },
+  {
+    "name": "Ladybug",
+    "desc": "Описание для Ladybug",
+    "image": "images/ladybug.PNG",
+    "price": 4814
+  },
+  {
+    "name": "Lego",
+    "desc": "Описание для Lego",
+    "image": "images/lego.PNG",
+    "price": 5007
+  },
+  {
+    "name": "Malifest",
+    "desc": "Описание для Malifest",
+    "image": "images/malifest.PNG",
+    "price": 5085
+  },
+  {
+    "name": "Matros",
+    "desc": "Описание для Matros",
+    "image": "images/matros.PNG",
+    "price": 7225
+  },
+  {
+    "name": "Maybl",
+    "desc": "Описание для Maybl",
+    "image": "images/maybl.PNG",
+    "price": 7242
+  },
+  {
+    "name": "Mikiandmaus",
+    "desc": "Описание для Mikiandmaus",
+    "image": "images/mikiandmaus.PNG",
+    "price": 6237
+  },
+  {
+    "name": "Mikimaus",
+    "desc": "Описание для Mikimaus",
+    "image": "images/Mikimaus.PNG",
+    "price": 5554
+  },
+  {
+    "name": "Майнкрафт",
+    "desc": "Тюринг полная игра",
+    "image": "images/minicraftblyat.PNG",
+    "price": 4500
+  },
+  {
+    "name": "Minions",
+    "desc": "Описание для Minions",
+    "image": "images/minions.PNG",
+    "price": 7658
+  },
+  {
+    "name": "Naruto",
+    "desc": "Описание для Naruto",
+    "image": "images/naruto.PNG",
+    "price": 7376
+  },
+  {
+    "name": "Neznayka",
+    "desc": "Описание для Neznayka",
+    "image": "images/neznayka.PNG",
+    "price": 6436
+  },
+  {
+    "name": "Patrul",
+    "desc": "Описание для Patrul",
+    "image": "images/patrul.PNG",
+    "price": 7931
+  },
+  {
+    "name": "Пьеро",
+    "desc": "Дед инсайд с ссср",
+    "image": "images/piero.PNG",
+    "price": 6500
+  },
+  {
+    "name": "Pirat",
+    "desc": "Описание для Pirat",
+    "image": "images/pirat.PNG",
+    "price": 7209
+  },
+  {
+    "name": "Pony",
+    "desc": "Описание для Pony",
+    "image": "images/pony.PNG",
+    "price": 5876
+  },
+  {
+    "name": "Футболист",
+    "desc": "Отец роналдо",
+    "image": "images/ronaldo.PNG",
+    "price": 5000
+  },
+  {
+    "name": "Rusalochka",
+    "desc": "Описание для Rusalochka",
+    "image": "images/rusalochka.PNG",
+    "price": 6541
+  },
+  {
+    "name": "Scauts",
+    "desc": "Описание для Scauts",
+    "image": "images/scauts.PNG",
+    "price": 4903
+  },
+  {
+    "name": "Шрек",
+    "desc": "Кто проживает на дне болота",
+    "image": "images/shrek.PNG",
+    "price": 8000
+  },
+  {
+    "name": "Soldat",
+    "desc": "Описание для Soldat",
+    "image": "images/soldat.PNG",
+    "price": 6029
+  },
+  {
+    "name": "Sonik",
+    "desc": "Описание для Sonik",
+    "image": "images/sonik.PNG",
+    "price": 7761
+  },
+  {
+    "name": "Spiderman",
+    "desc": "Описание для Spiderman",
+    "image": "images/spiderman.PNG",
+    "price": 6253
+  },
+  {
+    "name": "Superkot",
+    "desc": "Описание для Superkot",
+    "image": "images/superkot.PNG",
+    "price": 7291
+  },
+  {
+    "name": "Супермен",
+    "desc": "Лунтик в плаще",
+    "image": "images/superpidor.PNG",
+    "price": 5500
+  },
+  {
+    "name": "Svinkapepa",
+    "desc": "Описание для Svinkapepa",
+    "image": "images/svinkapepa.PNG",
+    "price": 6986
+  },
+  {
+    "name": "Teremok",
+    "desc": "Описание для Teremok",
+    "image": "images/teremok.PNG",
+    "price": 4722
+  },
+  {
+    "name": "Thor",
+    "desc": "Описание для Thor",
+    "image": "images/thor.PNG",
+    "price": 4955
+  },
+  {
+    "name": "Tiktokers",
+    "desc": "Описание для Tiktokers",
+    "image": "images/tiktokers.PNG",
+    "price": 5596
+  },
+  {
+    "name": "Железный Человек",
+    "desc": "Гений, миллиардер, плейбой, филантроп",
+    "image": "images/tonysrark.PNG",
+    "price": 6000
+  },
+  {
+    "name": "Transformers",
+    "desc": "Описание для Transformers",
+    "image": "images/transformers.PNG",
+    "price": 6985
+  },
+  {
+    "name": "Trikotaa",
+    "desc": "Описание для Trikotaa",
+    "image": "images/trikotaa.PNG",
+    "price": 5477
+  },
+  {
+    "name": "Wednesday",
+    "desc": "Описание для Wednesday",
+    "image": "images/wednesday.PNG",
+    "price": 6756
+  }
 ];
 
 const showsData = [
   {
-    name: "Шоу мыльных пузырей",
-    desc: "Гигантские пузыри, огненные пузыри и дети внутри пузыря.",
-    price: "1400 AED",
-    age: "2-7 лет",
-    duration: "30 минут",
-    video: "videos/Foam.mp4"
+    "name": "Шоу мыльных пузырей",
+    "desc": "Гигантские пузыри, огненные пузыри и дети внутри пузыря.",
+    "price": "1400 AED",
+    "age": "2-7 лет",
+    "duration": "30 минут",
+    "video": "videos/Foam.mp4",
+    "image": "images/bubble.jpg"
   },
   {
-    name: "Неоновое шоу пузырей",
-    desc: "Светящиеся в темноте пузыри, тоннели и световые эффекты.",
-    price: "1800 AED",
-    age: "5-12 лет",
-    duration: "30 минут",
-    video: "videos/Bubble.mp4"
+    "name": "Неоновое шоу пузырей",
+    "desc": "Светящиеся в темноте пузыри, тоннели и световые эффекты.",
+    "price": "1800 AED",
+    "age": "5-12 лет",
+    "duration": "30 минут",
+    "video": "videos/Bubble.mp4",
+    "image": "images/bubble.jpg"
   },
   {
-    name: "Химическое шоу",
-    desc: "Огненные и паровые эксперименты, сухой лёд и яркие опыты.",
-    price: "1400 AED",
-    age: "5-15 лет",
-    duration: "30 минут",
-    video: "videos/Chemical.mp4"
+    "name": "Химическое шоу",
+    "desc": "Огненные и паровые эксперименты, сухой лёд и яркие опыты.",
+    "price": "1400 AED",
+    "age": "5-15 лет",
+    "duration": "30 минут",
+    "video": "videos/Chemical.mp4",
+    "image": "images/chemical.jpg"
   },
   {
-    name: "Тесла шоу",
-    desc: "Электрические разряды, молнии и безопасные эксперименты.",
-    price: "1400 AED",
-    age: "6-15 лет",
-    duration: "30 минут",
-    video: "videos/Tesla.mp4"
+    "name": "Тесла шоу",
+    "desc": "Электрические разряды, молнии и безопасные эксперименты.",
+    "price": "1400 AED",
+    "age": "6-15 лет",
+    "duration": "30 минут",
+    "video": "videos/Tesla.mp4",
+    "image": "images/tesla.jpg"
   },
   {
-    name: "Бумажное шоу",
-    desc: "Танцы, музыка и буря из бумаги — яркое праздничное веселье.",
-    price: "2000 AED",
-    age: "5-9 лет",
-    duration: "30 минут",
-    video: "videos/Paper.mp4"
+    "name": "Бумажное шоу",
+    "desc": "Танцы, музыка и буря из бумаги — яркое праздничное веселье.",
+    "price": "2000 AED",
+    "age": "5-9 лет",
+    "duration": "30 минут",
+    "video": "videos/Paper.mp4",
+    "image": "images/paper.jpg"
   },
   {
-    name: "Крио-шоу с мороженым",
-    desc: "Зрелищные эксперименты с холодом + мороженое для детей.",
-    price: "1800 AED",
-    age: "5-15 лет",
-    duration: "45 минут",
-    video: "videos/Icecream.mp4"
+    "name": "Крио-шоу с мороженым",
+    "desc": "Зрелищные эксперименты с холодом + мороженое для детей.",
+    "price": "1800 AED",
+    "age": "5-15 лет",
+    "duration": "45 минут",
+    "video": "videos/Icecream.mp4",
+    "image": "images/cryo.jpg"
   },
   {
-    name: "Слайм шоу-мастерская",
-    desc: "Создай свой слайм — яркие цвета, весёлое творчество.",
-    price: "1000 AED (до 10 детей)",
-    age: "5-10 лет",
-    duration: "30-45 минут",
-    video: "videos/slime-show.mov"
+    "name": "Слайм шоу-мастерская",
+    "desc": "Создай свой слайм — яркие цвета, весёлое творчество.",
+    "price": "1000 AED (до 10 детей)",
+    "age": "5-10 лет",
+    "duration": "30-45 минут",
+    "video": "videos/slime-show.mov",
+    "image": "images/slime.jpg"
   },
   {
-    name: "Мастерская блокнотов",
-    desc: "Творческий мастер-класс по созданию блокнотов со стикерами и декором.",
-    price: "1200 AED (до 10 детей)",
-    age: "5-10 лет",
-    duration: "30-45 минут",
-    video: "videos/Notebook.mp4"
+    "name": "Мастерская блокнотов",
+    "desc": "Творческий мастер-класс по созданию блокнотов со стикерами и декором.",
+    "price": "1200 AED (до 10 детей)",
+    "age": "5-10 лет",
+    "duration": "30-45 минут",
+    "video": "videos/Notebook.mp4",
+    "image": "images/notebook.jpg"
   },
   {
-    name: "Пенная вечеринка",
-    desc: "Огромная пена, музыка и танцы в облаке пузырей.",
-    price: "2200 AED",
-    age: "5-15 лет",
-    duration: "60 минут",
-    video: "videos/Foam.mp4"
+    "name": "Пенная вечеринка",
+    "desc": "Огромная пена, музыка и танцы в облаке пузырей.",
+    "price": "2200 AED",
+    "age": "5-15 лет",
+    "duration": "60 минут",
+    "video": "videos/Foam.mp4",
+    "image": "images/foam.jpg"
   },
   {
-    name: "Магическое шоу",
-    desc: "Весёлые фокусы, иллюзии и интерактив с детьми.",
-    price: "1400 AED",
-    age: "4-7 лет",
-    duration: "30 минут",
-    video: "videos/Magic.mp4"
+    "name": "Магическое шоу",
+    "desc": "Весёлые фокусы, иллюзии и интерактив с детьми.",
+    "price": "1400 AED",
+    "age": "4-7 лет",
+    "duration": "30 минут",
+    "video": "videos/Magic.mp4",
+    "image": "images/magic.jpg"
   },
   {
-    name: "Сумасшедшая коробка с животными",
-    desc: "Интерактивный сюрприз с экзотическими животными.",
-    price: "1900 AED",
-    age: "5+",
-    duration: "40 минут",
-    video: "videos/Crazy.mp4"
+    "name": "Сумасшедшая коробка с животными",
+    "desc": "Интерактивный сюрприз с экзотическими животными.",
+    "price": "1900 AED",
+    "age": "5+",
+    "duration": "40 минут",
+    "video": "videos/Crazy.mp4",
+    "image": "images/crazybox.jpg"
   },
   {
-    name: "Мафия для детей и подростков",
-    desc: "Ролевая игра, логика и весёлый сторителлинг.",
-    price: "700 AED",
-    age: "6+",
-    duration: "60 минут",
-    video: "videos/Mafia.mp4"
+    "name": "Мафия для детей и подростков",
+    "desc": "Ролевая игра, логика и весёлый сторителлинг.",
+    "price": "700 AED",
+    "age": "6+",
+    "duration": "60 минут",
+    "video": "videos/Mafia.mp4",
+    "image": "images/mafia.jpg"
   },
   {
-    name: "Мафия для взрослых",
-    desc: "Детективная ролевая игра в стиле вечеринки.",
-    price: "900 AED",
-    age: "16+",
-    duration: "60 минут",
-    video: "videos/MagicAdults.mp4"
+    "name": "Мафия для взрослых",
+    "desc": "Детективная ролевая игра в стиле вечеринки.",
+    "price": "900 AED",
+    "age": "16+",
+    "duration": "60 минут",
+    "video": "videos/MagicAdults.mp4",
+    "image": "images/mafia.jpg"
   },
   {
-    name: "Ведущий для подростков",
-    desc: "Игры, конкурсы и зажигательный ведущий для вечеринки.",
-    price: "700 AED",
-    age: "12-18 лет",
-    duration: "60 минут",
-    video: "videos/teen.mp4"
+    "name": "Ведущий для подростков",
+    "desc": "Игры, конкурсы и зажигательный ведущий для вечеринки.",
+    "price": "700 AED",
+    "age": "12-18 лет",
+    "duration": "60 минут",
+    "video": "videos/teen.mp4",
+    "image": "images/default-show.jpg"
   }
 ];
-
 
 const masterClassesData = [
   { name: "Создание костюмов", desc: "Научитесь создавать костюмы своими руками", price: 2500, icon: "✂️" },
@@ -196,7 +491,6 @@ function initSliders() {
         <h4>${character.name}</h4>
         <p>${character.desc}</p>
         ${currentPackage === 'custom' ? `<p class="price-tag">${character.price}₽</p>` : ''}
-        
       </div>
     `;
     
@@ -217,7 +511,7 @@ function initSliders() {
       <div class="show-info">
         <h4>${show.name}</h4>
         <p>${show.desc}</p>
-        ${currentPackage === 'custom' ? `<p class="price-tag">${show.price}₽</p>` : ''}
+        ${currentPackage === 'custom' ? `<p class="price-tag">${show.price}</p>` : ''}
         <button class="view-btn" data-video="${show.video}" data-name="${show.name}">Посмотреть</button>
       </div>
     `;
@@ -738,46 +1032,11 @@ function initCarouselNavigation() {
 }
 
 function showCaseModal(index) {
-  const caseData = casesData[index];
-  const modal = document.getElementById('case-modal');
-  const modalContent = document.getElementById('case-modal-content');
-  
-  let html = `
-    <h3>${caseData.title}</h3>
-    <p class="case-modal-description">${caseData.description}</p>
-    <div class="case-modal-details">${caseData.details}</div>
-    <div class="case-modal-gallery">
-  `;
-  
-  caseData.images.forEach(img => {
-    html += `<img src="${img}" alt="${caseData.title}" class="case-modal-image">`;
-  });
-  
-  html += '</div>';
-  
-  modalContent.innerHTML = html;
-  modal.classList.add('active');
+  // Реализация показа модального окна с кейсами
+  console.log('Показать кейс', index);
 }
 
 function showProductModal(index) {
-  const productData = productsData[index];
-  const modal = document.getElementById('case-modal');
-  const modalContent = document.getElementById('case-modal-content');
-  
-  let html = `
-    <h3>${productData.name}</h3>
-    <p class="case-modal-description">${productData.description}</p>
-    <div class="product-modal-price">${productData.price}</div>
-    <div class="case-modal-details">${productData.details}</div>
-    <div class="case-modal-gallery">
-  `;
-  
-  productData.images.forEach(img => {
-    html += `<img src="${img}" alt="${productData.name}" class="case-modal-image">`;
-  });
-  
-  html += '</div>';
-  
-  modalContent.innerHTML = html;
-  modal.classList.add('active');
+  // Реализация показа модального окна с продуктами
+  console.log('Показать продукт', index);
 }
